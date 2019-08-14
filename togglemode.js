d3d72b35159fb196ec236b993c8ac29f9ed80776
ahmed <dickagonia@gmail.com>
2019-08-14 09:40:42 -0400

    import from code pen

---
+++ togglemode.js
@@ -0,0 +1,5 @@
+function toggleMode() {
+	var body = document.getElementById("body");
+	var currentClass = body.className;
+	body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}
