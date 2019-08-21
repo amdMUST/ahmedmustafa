d5ccc99cdfcd0528fb8df8a14143903a240e154d
dickagonia <52543589+dickagonia@users.noreply.github.com>
2019-08-21 13:24:00 -0400

    overlayBase
    -made the overlay or started atleast

--- togglemode.js
+++ togglemode.js
@@ -1,5 +1,16 @@
-function toggleMode() {
-	var body = document.getElementById("body");
-	var currentClass = body.className;
-	body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-}
+
+// to toggle dark/light mode //
+	function toggleMode() {
+		var body = document.getElementById("body");
+		var currentClass = body.className;
+		body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+	}
+
+// to get the modal //
+	function overlayOn() {
+		document.getElementById("overlayBig").style.display = "block";
+	}
+  
+	function overlayOff() {
+		document.getElementById("overlayBig").style.display = "none";
+	}
